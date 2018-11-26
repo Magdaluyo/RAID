da8d6095b1c79b4260498d60fedcaf02afcf13db
Manuel Alejandro Sanchez <ManuelAlejandro@Manuels-MacBook-Air.local>
2018-11-26 14:49:19 -0800

    Attempting to add login feature

--- script.js
+++ script.js
@@ -150,3 +150,21 @@ db.collection("tickets").orderBy("created")
     }); 
 }
 
+   /* ******** ATTEMPTING LOGIN ******** */
+
+    // Get elements
+    const txtEmail = document.getElementById("userEmail");
+    const txtPassword = document.getElementById("userPassword");
+    const btnLogin = document.getElementById("btnLogin");
+    
+    // Add login event
+    btnLogin.addEventListener('click', e => {
+        // Get email and password
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        // Sign in
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+    });
+    
