1925a78dc08134ac957ade6b35f690749d90bdf6
Dom <dom@Doms-MacBook-Pro.local>
2018-11-26 10:56:40 -0800

    More tester changes

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ db.collection("tickets").where("reporter", "==", "dfh5lXUrkYMpGRfzDatc").orderBy
         console.log("Error getting documents: ", error);
     });
 
-const report_list = document.querySelector("report_list");
+const report_list = document.querySelector("#report_list");
 
 db.collection("tickets").orderBy("created")
     .get()
@@ -72,6 +72,7 @@ db.collection("tickets").orderBy("created")
 				report_list.innerHTML += "<div class='w3-container w3-card w3-white w3-round w3-margin'>";
 				report_list.innerHTML += "<h6><i class='fa fa-bug'></i>   Bug Ticket " + doc.id + "</h6>";
         		report_list.innerHTML += "<p>Software: " + doc.data().software + " </p>";
+				report_list.innerHTML += "<p>Reported: " + doc.data().created + "</p>";
         		report_list.innerHTML += "<p>Problem: " + doc.data().problem + "</p>";
         		report_list.innerHTML += "<p>Description: " + doc.data().description + "</p>";
         		report_list.innerHTML += "<p>Type of Error: " + doc.data().typeoferror + "</p>";
@@ -97,11 +98,28 @@ db.collection("tickets").orderBy("created")
                 tester_list.innerHTML += "<div class='w3-container w3-card w3-white w3-round w3-margin'>";
                 tester_list.innerHTML += "<h6><i class='fa fa-bug'></i>   Bug Ticket " + doc.id + "</h6>";
                 tester_list.innerHTML += "<p>Software: " + doc.data().software + " </p>";
+				tester_list.innerHTML += "<p>Reported: " + doc.data().created + "</p>";
                 tester_list.innerHTML += "<p>Problem: " + doc.data().problem + "</p>";
                 tester_list.innerHTML += "<p>Description: " + doc.data().description + "</p>";
                 tester_list.innerHTML += "<p>Type of Error: " + doc.data().typeoferror + "</p>";
                 tester_list.innerHTML += "<p>Status of Bug: " + doc.data().status + "</p></div>";
-                tester_list.innerHTML += "<hr class='w3-clear'>";
+/*
+				tester_list.innerHTML += "<label for='Priotities'>Priorities</label>";
+				tester_list.innerHTML += "<select id = 'Priorities' name='Priorities' class='w3-button w3-theme-d1 w3-margin-bottom'><i class='fa fa-sort-amount-desc'></i>";
+                tester_list.innerHTML += "<option value = 'choosepriority'> --Update Priority of Bug-- </option>";
+                tester_list.innerHTML += "<option value = 'Severe'> Severe </option>";
+                tester_list.innerHTML += "<option value = 'Moderate'> Moderate </option>";
+                tester_list.innerHTML += "<option value = 'Low'> Low </option> </select>";
+
+				tester_list.innerHTML += "<label for='Status'>Status</label>";
+				tester_list.innerHTML += "<select id = 'Status' name='Status' class='w3-button w3-theme-d1 w3-margin-bottom'><i class='fa fa-asterisk'></i>";
+                tester_list.innerHTML += "<option value = 'chooseStatus'> --Update Status of Bug-- </option>";
+                tester_list.innerHTML += "<option value = 'Fix in Progress'> Fix in Progress </option>";
+                tester_list.innerHTML += "<option value = 'Ready for Deployment'> Ready for Deployment </option> </select>";
+
+				tester_list.innerHTML += "<button onclick=document.getElementById('id01').style.display='block' class='w3-button w3-theme-d1 w3-margin-bottom'>Update Notes</button>";
+*/
+				tester_list.innerHTML += "<hr class='w3-clear'>";
             });
         }
         else {
